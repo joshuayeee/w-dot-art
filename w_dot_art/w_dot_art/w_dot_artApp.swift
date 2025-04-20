5f7d52a87a8610ffc0d8a1d4d5941159715563ca
mnchow98 <mnchow98@csu.fullerton.edu>
2025-04-20 14:52:19 -0700

    test

---
+++ w_dot_art/w_dot_art/w_dot_artApp.swift
@@ -0,0 +1,17 @@
+//
+//  w_dot_artApp.swift
+//  w_dot_art
+//
+//  Created by csuftitan on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct w_dot_artApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
