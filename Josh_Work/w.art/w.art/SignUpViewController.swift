106f3a259f830c002a5f08fcac3b157248a2044c
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2025-04-27 01:46:34 -0700

    added sign up and log in views

---
+++ Josh_Work/w.art/w.art/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  w.art
+//
+//  Created by Joshua Yee on 4/27/25.
+//
+
+import UIKit
+
+class SignUpViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("lol")
+    }
+    
+}
