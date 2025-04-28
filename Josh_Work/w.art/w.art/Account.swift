b105b7ccd77600ec5927c67bda4ed5caa00d6e2a
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2025-04-28 16:26:11 -0700

    new jump sound

---
+++ Josh_Work/w.art/w.art/Account.swift
@@ -0,0 +1,32 @@
+//
+//  Account.swift
+//  w.art
+//
+//  Created by Joshua Yee on 4/28/25.
+//
+
+class Account {
+    
+    private let my_username: String
+    private let my_password: String
+    private let my_email: String
+    
+    init(username: String, password: String, email: String) {
+        self.my_username = username
+        self.my_password = password
+        self.my_email = email
+    }
+    
+    func get_username() -> String {
+        return my_username
+    }
+    
+    func get_password() -> String {
+        return my_password
+    }
+    
+    func get_email() -> String {
+        return my_email
+    }
+    
+}
