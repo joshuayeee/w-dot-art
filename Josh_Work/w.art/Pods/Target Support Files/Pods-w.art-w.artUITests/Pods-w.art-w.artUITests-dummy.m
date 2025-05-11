f1b804cfcd1f70636abc6cbb771b2f7b26e7be1b
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2025-05-11 00:24:27 -0700

    gutted main

--- Josh_Work/w.art/Pods/Target Support Files/Pods-w.art-w.artUITests/Pods-w.art-w.artUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_w_art_w_artUITests : NSObject
-@end
-@implementation PodsDummy_Pods_w_art_w_artUITests
-@end
--- w_dot_art/w_dot_art/Product_Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Product_Model.swift
-//  w_dot_art
-//
-//  Created by csuftitan on 4/20/25.
-//
-
-import Foundation
